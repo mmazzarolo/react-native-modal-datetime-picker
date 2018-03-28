1dab59407da937ab8a17a6b990913c28c560acbb
Grzegorz Mandziak <alimek@alimek.pl>
2018-03-28 10:52:20 +0200

    add brackets to return

--- src/CustomDatePickerIOS/index.style.js
+++ src/CustomDatePickerIOS/index.style.js
@@ -3,10 +3,12 @@ import { StyleSheet, Dimensions, Platform } from "react-native";
 export const isIphoneX = () => {
   const { height, width } = Dimensions.get("window");
 
-  return Platform.OS === "ios" &&
+  return (
+    Platform.OS === "ios" &&
     !Platform.isPad &&
     !Platform.isTVOS &&
-    (height === 812 || width === 812);
+    (height === 812 || width === 812)
+  );
 };
 
 const BORDER_RADIUS = 13;
