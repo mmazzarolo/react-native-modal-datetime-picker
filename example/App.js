13d859737e5d20ed874b7cb6470426e96cd449fb
Vytenis Kuciauskas <projektas@gmail.com>
2020-04-20 12:27:04 +0300

    Add spinner example

--- example/App.js
+++ example/App.js
@@ -4,6 +4,7 @@ import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const App = () => {
   const [pickerMode, setPickerMode] = useState(null);
+  const [androidType, setAndroidType] = useState('default');
 
   const showDatePicker = () => {
     setPickerMode("date");
@@ -11,6 +12,10 @@ const App = () => {
 
   const showTimePicker = () => {
     setPickerMode("time");
+  };
+
+  const showDateTimePicker = () => {
+    setPickerMode("datetime");
   };
 
   const hidePicker = () => {
@@ -22,15 +27,26 @@ const App = () => {
     hidePicker();
   };
 
+  const toggleType = () => {
+    if (androidType === 'default') {
+      setAndroidType('spinner')
+    } else {
+      setAndroidType('default')
+    }
+  }
+
   return (
     <View style={style.root}>
       <Button title="Show Date Picker" onPress={showDatePicker} />
       <Button title="Show Time Picker" onPress={showTimePicker} />
+      <Button title="Show DateTime Picker" onPress={showDateTimePicker} />
+      <Button title={`Android only Toggle type ${androidType}`} onPress={toggleType} />
       <DateTimePickerModal
         isVisible={pickerMode !== null}
         mode={pickerMode}
         onConfirm={handleConfirm}
         onCancel={hidePicker}
+        display={androidType}
       />
     </View>
   );
