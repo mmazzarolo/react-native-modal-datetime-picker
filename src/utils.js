ca6763858a4e0aa664875c407ee8deb5eb30a72d
Óscar Carretero <ocarreterom@gmail.com>
2020-11-11 00:06:50 +0100

    update isIphoneX helper
    Update `isIphoneX` helper according to `react-native-iphone-x-helper`.
    
    Adds iPhone 12 and iPhone 12 compatibility.

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,15 @@ export const isIphoneX = () => {
     Platform.OS === "ios" &&
     !Platform.isPad &&
     !Platform.isTVOS &&
-    (height === 812 || width === 812 || height === 896 || width === 896)
+    (height === 780 ||
+      width === 780 ||
+      height === 812 ||
+      width === 812 ||
+      height === 844 ||
+      width === 844 ||
+      height === 896 ||
+      width === 896 ||
+      height === 926 ||
+      width === 926)
   );
 };
