1ae6145269704e748d355666920473020c81e049
Stas Shakirov <punksta@protonmail.com>
2021-10-04 10:30:40 +0200

    Update DateTimePickerModal.ios.js

--- src/DateTimePickerModal.ios.js
+++ src/DateTimePickerModal.ios.js
@@ -43,6 +43,7 @@ export class DateTimePickerModal extends React.PureComponent {
     isVisible: PropTypes.bool,
     pickerContainerStyleIOS: PropTypes.any,
     pickerStyleIOS: PropTypes.any,
+    backdropStyle: PropTypes.any,
     onCancel: PropTypes.func.isRequired,
     onConfirm: PropTypes.func.isRequired,
     onChange: PropTypes.func,
@@ -61,6 +62,7 @@ export class DateTimePickerModal extends React.PureComponent {
     isVisible: false,
     pickerContainerStyleIOS: {},
     pickerStyleIOS: {},
+    backdropStyle: {},
   };
 
   state = {
@@ -134,6 +136,7 @@ export class DateTimePickerModal extends React.PureComponent {
       onConfirm,
       onChange,
       onHide,
+      backdropStyle,
       ...otherProps
     } = this.props;
     const isAppearanceModuleAvailable = !!(
@@ -163,6 +166,7 @@ export class DateTimePickerModal extends React.PureComponent {
         contentStyle={[pickerStyles.modal, modalStyleIOS]}
         onBackdropPress={this.handleCancel}
         onHide={this.handleHide}
+        backdropStyle={backdropStyle}
         {...modalPropsIOS}
       >
         <View
